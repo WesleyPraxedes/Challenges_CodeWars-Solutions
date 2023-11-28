66990f29cbdfbbf3dcb7e83da80ccd5d71fb8c34
Wesley Praxedes <wesleyprofile@gmail.com>
2023-11-27 22:10:13 -0300

    Start project

---
+++ Challenges_CodeWars-Solutions/src/main/java/Challenges_CodeWars_Solutions/Challenges_CodeWars_Solutions/App.java
@@ -0,0 +1,13 @@
+package Challenges_CodeWars_Solutions.Challenges_CodeWars_Solutions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
